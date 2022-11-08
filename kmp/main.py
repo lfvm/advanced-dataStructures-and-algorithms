9d28f0dd061a8167af8a663215ce91e364a20db5
DiegoAraque21 <diego.araque.fernandez@gmail.com>
2022-11-07 20:50:53 -0600

    .

--- kmp/main.py
+++ kmp/main.py
@@ -5,14 +5,18 @@
 # Uriel Aguilar
 
 
-def kmp(mainString, pattern):
+def kmp(mainString, pattern, prevArr):
 
+    # longitudes del patron y el texto en el que buscaremos
     n = len(mainString)
     m = len(pattern)
 
+    # inicializamos los indices en 0
     i = 0
     j = 0
 
+    # el array con los indices en donde se encuentra el patron
+    # empieza vacio
     locArray = []
     
     while i < n:
@@ -24,17 +28,48 @@ def kmp(mainString, pattern):
 
             # si se llego al final del pattern y coinciden se encontro 
             if j == m:
+                # añadimos el indice en donde se encontro el patron
                 locArray.append(i-j)
-                j = 0
-           
+                # reiniciamos el indice del patron
+                j = prevArr[j-1]
+
+        # si no coinciden estos valores
         else:
-            # si coincide un caracter que no es el primero regresa el indice de j
-            if j > 0:
-                j -= 1
-            else: 
+            # si el indice j es diferente de 0 se reinicia
+            if j != 0:
+                j = prevArr[j-1]
+            # si es 0 se incrementa el indice del string 
+            # en el que estamos buscando
+            else:
                 i += 1
 
     return locArray
+
+
+def getPrevArr(pattern, length, prevArr, m):
+    # nuestro iterador empieza en 1
+    i = 1
+    # mientras nuestro contador sea menor a la longitud del patron
+    # seguiremos en este loop
+    while i < m:
+        # si el caracter en la posicion i es igual al caracter en la posicion length(que empieza en cero)
+        # incrementamos nuestra 1, nuestra longitud del prefijo y añadimos esta al prevArray 
+        if pattern[i] == pattern[length]:
+            length += 1
+            prevArr[i] = length
+            i += 1
+        # si no es igual, checamos si la longitud es mayor a 0.
+        # si ese es el caso la longitud del prefijo se reinicia
+        elif length != 0:
+            length = prevArr[length - 1]
+        # si es 0, el prefijo es 0 e incrementamos el contador
+        else:
+            prevArr[i] = 0
+            i += 1
+
+    return prevArr
+
+
                 
 
 if __name__ == "__main__":
@@ -42,9 +77,13 @@ if __name__ == "__main__":
     mainString = input("Ingresa la cadena de texto: ")
     pattern = input("Ingresa el patron que estas buscando: ")
 
-    locArray = kmp(mainString, pattern)
-    print(locArray)
+    # mainString = "101001101011100001111110001010101001100111110001001101011100011110110111000011101101110100110111101101011111101110001101110011000010000011100100011000011000000111101011110101101101100011111000000111000000010000011000010111111011001010011100000011101001111000110110111001100001111110001010001010111100101000100000101110010000111101101110111010101000101001010011101010100011000011100000000111100110100"
+    # pattern = "110001"
 
-        
+    # calculamos el array de prefijos con la funcion getPrevArr
+    prevArr = getPrevArr(pattern, 0, [i for i in range(len(pattern))], len(pattern))
+
+    locArray = kmp(mainString, pattern, prevArr)
+    print(locArray)
 
   
